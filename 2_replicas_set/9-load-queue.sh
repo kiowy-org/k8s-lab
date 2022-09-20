4b9e8fbcdd8b96d09b37bf6558ece0bdad9f145c
Jerome Massard <jerome.massard@ov-corporation.com>
2022-09-20 14:41:01 +0200

    2_replicas_set/9-load-queue.sh

--- 2_replicas_set/9-load-queue.sh
+++ 2_replicas_set/9-load-queue.sh
@@ -7,18 +7,3 @@ for i in work-item-{0..99}; do
   curl -X POST localhost:8080/memq/server/queues/keygen/enqueue \
     -d "$i"
 done
-
-
-kubectl apply -f 7-rs-queue.yaml
-kubectl apply -f 8-service-queue.yaml
-
-QUEUE_POD=$(kubectl get pods -l app=work-queue,component=queue \
--o jsonpath='{.items[0].metadata.name}')
-kubectl port-forward $QUEUE_POD 8080:8080
-# Garder ce terminal actif !
-
-# Éxecuter 9-load-queue.sh
-./9-load-queue.sh
-curl 127.0.0.1:8080/memq/server/stats
-
-kubectl apply -f 10-job-consumer.yaml
